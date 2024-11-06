4b1629f6326cc87c4c862b182dd9dfef1f5cb395
AngelaBriel <abriel@suse.com>
2024-11-06 16:43:29 +0100

    change THP from 'never' to 'madvise' (bsc#1232373)

--- lib/sc_hooks.sh
+++ lib/sc_hooks.sh
@@ -33,15 +33,26 @@ lc_add() {
     sed -i 's/^# Set to 65536/# Set to 1048576 (as recommended by revision 6 of the SAP Note)/' "$SN"
 }
 
+thp_change() {
+    sed -i 's/^THP=never/THP=madvise/g' "$SN"
+    sed -i "s/^# set to 'never'/# set to 'madvise'/g" "$SN"
+    sed -i '/^# Disable transparent hugepages/i\
+# '\''madvise'\'' will enter direct reclaim like '\''always'\'' but only for regions that\
+# are have used madvise(MADV_HUGEPAGE). This is the default behaviour.' "$SN"
+    sed -i 's/^# Disable transparent hugepages/# Configure transparent hugepages/' "$SN"
+}
+
 case "$hook_opt" in
 add)
     add_ignore
     lc_add
+    thp_change
     ;;
 del)
     del_ignore
     ;;
 nothing)
     lc_add
+    thp_change
     ;;
 esac
